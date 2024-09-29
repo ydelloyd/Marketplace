dc5c370b0b3a1e3d4166f017ddd031ba64e1bf8c
York Delloyd <ydelloyd@gmail.com>
2024-09-29 18:10:59 -0400

    Added some initial UI components and static test data.

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,19 +2,40 @@
 import { CssBaseline, Container } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export function App() {
   return (
     <div>
       <CssBaseline />
-	  <Container>
-	  	<BrowserRouter>
-			<Routes>
-				<Route element={<HomePage />} path="/" />
-			</Routes>
-		</BrowserRouter>
-	  </Container>
-	</div>
+	  <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              The Best Marketplace
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      <Container maxWidth={false}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<HomePage />} path="/" />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </div>
   );
 }
 
--- frontend/src/app/components/HomePage.tsx
+++ frontend/src/app/components/HomePage.tsx
@@ -1,22 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import Grid from '@mui/material/Grid2';
+import { Job } from "../models/jobModel";
+import { OwnerModel } from "../models/ownerModel";
 
 const HomePage: React.FC = () => {
-  return (
-    <div>
-      <Typography variant="h3">Jobs</Typography>
 
-      <Button
-        component={Link}
-        to="/post-job"
-        variant="contained"
-        color="primary"
-        sx={{ marginTop: 4 }}
-      >
-        Job Button
-      </Button>
-    </div>
+  const jobs: Job[] = [
+    {
+      id: "1",
+      title: "First Job",
+      description: "first job description",
+      owner: { name: "TEST", contactInfo: "test@test.com"} as OwnerModel,
+      expiration: new Date().toISOString(), // ISO 8601 date-time string
+      lowestBid: 1,
+      numberOfBids: 5,
+      reqirements: "skills",
+      createdAt: new Date().toISOString()
+    },
+    {
+      id: "2",
+      title: "Second Job",
+      description: "second job description",
+      owner: { name: "TEST", contactInfo: "test@test.com"} as OwnerModel,
+      expiration: new Date().toISOString(), // ISO 8601 date-time string
+      lowestBid: 1,
+      numberOfBids: 5,
+      reqirements: "skills",
+      createdAt: new Date().toISOString()
+    }
+  ];
+
+  return (
+    <Grid container spacing={2}>
+      <Grid size={12}>
+        <Typography variant="h3">Jobs</Typography>
+      </Grid>
+      {jobs.map((job) => {
+        return (
+          <Grid size={4}>
+            <Typography variant="h5">{job.title}</Typography>
+            <Typography variant="body1">{job.description}</Typography>
+            <Typography variant="body1">{job.owner.name}</Typography>
+            <Typography variant="body1">{job.owner.contactInfo}</Typography>
+            <Typography variant="body1">{job.expiration}</Typography>
+            <Typography variant="body1">{job.lowestBid}</Typography>
+            <Typography variant="body1">{job.numberOfBids}</Typography>
+            <Typography variant="body1">{job.reqirements}</Typography>
+            <Typography variant="body1">{job.createdAt}</Typography>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
 
